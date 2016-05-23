e4a578332e401e350d5fad3e042976532740cd94
Filipe Munhoz <filipemunhoz@gmail.com>
2016-05-23 18:50:43 -0300

    Update customFilters.js

--- customFilters.js
+++ customFilters.js
@@ -17,3 +17,29 @@ angular.module("customFilters", [])
 			}
 		}
 	})
+
+	.filter("uniqueDate", function ($filter) {
+		return function (data, propertyName) {
+			//console.log(angular.isArray(data) + " - " + angular.isNumber(propertyName));
+			if (angular.isArray(data) ) {
+				var results = [];
+				var keys = {};
+					
+				for (var i = 0; i < data.length; i++) {
+					//var val = data[i][propertyName];
+					
+					var val = $filter('date')(new Date(data[i][propertyName] * 1000), 'dd/MM/yyyy', 'UTC');
+					//console.log(prop);
+					if (angular.isUndefined(keys[val])) {
+					//console.log(val);
+						keys[val] = true;
+						results.push(val);						
+					}
+				}
+				//console.log(results.length);
+				return results;
+			} else {
+				return data;
+			}
+		}
+	})	 
